5e39c0079ed51d007ad6b43ff7c03e6166605c72
Abdullah <147026901+dddprojects@users.noreply.github.com>
2023-10-08 11:52:42 +0800

    Update HeroSection.jsx

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -22,7 +22,7 @@ const HeroSection = () => {
             <br></br>
             <TypeAnimation
               sequence={[
-                "Judy",
+                "Abdullah",
                 1000,
                 "Web Developer",
                 1000,
